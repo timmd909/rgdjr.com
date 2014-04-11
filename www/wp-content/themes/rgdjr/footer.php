a7e9f6a1606b210913e4f4befda97d196773a6ed
Tim Doerzbacher <tim@tim-doerzbacher.com>
2014-04-11 23:30:43 +0000

    Added in a shoutoff for me

--- www/wp-content/themes/rgdjr/footer.php
+++ www/wp-content/themes/rgdjr/footer.php
@@ -2,10 +2,12 @@
 	</div><!-- #main -->
 
 	<footer>
-		&copy; 2014 &mdash; Rob Doerzbacher
+		<div class="alignleft">&copy; 2014 &mdash; Rob Doerzbacher</div>
+		<div class="alignright">Site by <a href="http://tim-doerzbacher.com" target="_blank">Tim Doerzbacher</a></div>
 	</footer>
 
 </div><!-- #page -->
+<!-- Thank you, WordPress, for being awesome! -->
 <?php wp_footer(); ?>
 </body>
 </html>
--- www/wp-content/themes/rgdjr/inc/header-common.php
+++ www/wp-content/themes/rgdjr/inc/header-common.php
@@ -6,15 +6,15 @@
 			<h2 class="site-description"><?php bloginfo('description'); ?></h2>
 			<?php rgdjr_wp_header_links(); ?>
 			<ul class="social-links">
-				<li><a href="mailto:rob@rgdjr.com">
+				<li><a href="mailto:rob@rgdjr.com" target="_blank">
 					<img src="/wp-content/themes/rgdjr/images/email.png" /></a></li>
-				<li><a href="https://twitter.com/RobDoerzbacher">
+				<li><a href="https://twitter.com/RobDoerzbacher" target="_blank">
 					<img src="/wp-content/themes/rgdjr/images/twitter.png" title="Twitter"/></a></li>
-				<li><a href="http://www.linkedin.com/pub/rob-doerzbacher/5/86a/b39">
+				<li><a href="http://www.linkedin.com/pub/rob-doerzbacher/5/86a/b39" target="_blank">
 					<img src="/wp-content/themes/rgdjr/images/linkedin.png" title="Linked In" /></a></li>
-				<li><a href="http://instagram.com/rgdjr">
+				<li><a href="http://instagram.com/rgdjr" target="_blank">
 					<img src="/wp-content/themes/rgdjr/images/instagram.png" title="Instagram" /></a></li>
-				<li><a href="https://www.youtube.com/channel/UCKeTYUmHZJUonaRI_IO5rYQ">
+				<li><a href="https://www.youtube.com/channel/UCKeTYUmHZJUonaRI_IO5rYQ" target="_blank">
 					<img src="/wp-content/themes/rgdjr/images/youtube.png" title="YouTube" /></a></li>
 			</ul>
 		</div>
